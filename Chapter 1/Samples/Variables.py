ec80d384a9696a626f16b71dc5664b93af655907
Daniel H Kemper II <danthemank@gmail.com>
2018-10-11 01:50:34 -0400

    Update Variables Sample
    Update Variables Sample for valid 3.7.0 syntax

--- Chapter 1/Samples/Variables.py
+++ Chapter 1/Samples/Variables.py
@@ -3,11 +3,11 @@ counter = 100          # An integer assignment
 miles = 1000.0       # A floating point
 name = "John"       # A string
 
-print counter
-print counter / 3
-print miles
-print miles / 3
-print name
+print(counter)
+print(counter / 3)
+print(miles)
+print(miles / 3)
+print(name)
 
 ###################
 
@@ -25,12 +25,12 @@ print name
 # Strings
 # str = 'Hello World!'
 #
-# print str          # Prints complete string
-# print str[0]       # Prints first character of the string
-# print str[2:5]     # Prints characters starting from 3rd to 5th
-# print str[2:]      # Prints string starting from 3rd character
-# print str * 2      # Prints string two times
-# print str + "TEST" # Prints concatenated string
+# print (str)          # Prints complete string
+# print (str[0])       # Prints first character of the string
+# print (str[2:5])     # Prints characters starting from 3rd to 5th
+# print (str[2:])      # Prints string starting from 3rd character
+# print (str * 2)      # Prints string two times
+# print (str + "TEST") # Prints concatenated string
 
 ###################
 
@@ -38,12 +38,12 @@ print name
 # list = [ 'abcd', 786 , 2.23, 'john', 70.2 ]
 # tinylist = [123, 'john']
 #
-# print list          # Prints complete list
-# print list[0]       # Prints first element of the list
-# print list[1:3]     # Prints elements starting from 2nd till 3rd
-# print list[2:]      # Prints elements starting from 3rd element
-# print tinylist * 2  # Prints list two times
-# print list + tinylist # Prints concatenated lists
+# print (list)          # Prints complete list
+# print (list[0])       # Prints first element of the list
+# print (list[1:3])     # Prints elements starting from 2nd till 3rd
+# print (list[2:])      # Prints elements starting from 3rd element
+# print (tinylist * 2)  # Prints list two times
+# print (list + tinylist) # Prints concatenated lists
 
 ###################
 
@@ -51,12 +51,12 @@ print name
 # tuple = ( 'abcd', 786 , 2.23, 'john', 70.2  )
 # tinytuple = (123, 'john')
 #
-# print tuple           # Prints complete list
-# print tuple[0]        # Prints first element of the list
-# print tuple[1:3]      # Prints elements starting from 2nd till 3rd
-# print tuple[2:]       # Prints elements starting from 3rd element
-# print tinytuple * 2   # Prints list two times
-# print tuple + tinytuple # Prints concatenated lists
+# print (tuple)           # Prints complete list
+# print (tuple[0])        # Prints first element of the list
+# print (tuple[1:3])      # Prints elements starting from 2nd till 3rd
+# print (tuple[2:])       # Prints elements starting from 3rd element
+# print (tinytuple * 2)   # Prints list two times
+# print (tuple + tinytuple) # Prints concatenated lists
 
 ###################
 
@@ -66,8 +66,8 @@ print name
 # dict[2]     = "This is two"
 # tinydict = {'name': 'john','code':6734, 'dept': 'sales'}
 #
-# print dict['one']       # Prints value for 'one' key
-# print dict[2]           # Prints value for 2 key
-# print tinydict          # Prints complete dictionary
-# print tinydict.keys()   # Prints all the keys
-# print tinydict.values() # Prints all the values
+# print (dict['one'])       # Prints value for 'one' key
+# print (dict[2])           # Prints value for 2 key
+# print (tinydict)          # Prints complete dictionary
+# print (tinydict.keys())   # Prints all the keys
+# print (tinydict.values()) # Prints all the values
